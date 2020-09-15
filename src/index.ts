d5dd2727a10f66ed1dccab1c270f58a1fe3529fa
Moses Kim <crystal.galaxy.sailor@gmail.com>
2020-09-15 13:57:18 -0700

    creating a new block and get the latest block, get the new timestamp, adding to existing blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 "use strict";
 
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string => {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  };
 
   constructor(
     index: number,
@@ -24,7 +35,42 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "22323131231312", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
-console.log(blockchain);
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimestamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimestamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+
+  blockchain.push(newBlock);
+  return newBlock;
+};
+createNewBlock("Hello");
+
+const timeDifference = () => {
+  setTimeout(() => {
+    createNewBlock("bye");
+    console.log(blockchain);
+  }, 1000);
+};
+timeDifference();
 
 export {};
